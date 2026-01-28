96b580639e4fa92ba59f1acd5faf8f6c273bd6b4
(크탄) <newgag29@gmail.com>
2026-01-28 09:17:41 +0900

    플레이어 기능기본구현
    플레이어 이동 및 fps  시점 구현
    플레이어 관련 하이러아키 구조에대한 구현
    플레이어를 이루는 기본 클래스들에대한 기초 구현
    
    플레이어 무기스왑기능 기본구현
    
    이외 여러가지 테스트용 임시 프리팹과 씬들

---
+++ ProjectAIF/Assets/Player/Player_Level.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Level : MonoBehaviour
+{
+    int Current_EXP;
+    int Max_EXP;
+}
---
+++ ProjectAIF/Assets/Player/Test_Weapon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Weapon : MonoBehaviour, IWeapon
+{
+    public void Attack_test()
+    {
+        
+    }
+}
