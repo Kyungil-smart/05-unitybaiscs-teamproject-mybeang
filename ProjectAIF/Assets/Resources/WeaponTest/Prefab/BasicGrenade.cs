446fd2d6dc591c9bc03376adaac8d4baada7560c
hjchun78-cmd <hjchun78@gmail.com>
2026-01-29 22:10:42 +0900

    [create]WeaponStatus
    각 무기(소총, 권총, 수류탄)의 기본 성능 등을 구현한 에셋+스크립트

---
+++ ProjectAIF/Assets/Resources/WeaponTest/Prefab/BasicGrenade.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicGrenade : MonoBehaviour
+{
+    // 기본 수류탄 초기값
+    [Header("기본 수류탄")]
+    public int Damage = 50;
+    public float EffectRange = 10f; // 폭발 범위
+    public int Magazine = 3;
+    public float ChargeTime = 3f;
+
+    [Header("강화 제한")]
+    public int MinDamage = 25;
+    public int MaxDamage = 100;
+    public float MinRange = 10f;
+    public float MaxRange = 10f;
+    public int MinMagazine = 0;
+    public int MaxMagazine = 6;
+
+    // 설정
+    [Header("터지기")]
+    public float ExplosionForce = 700f; // 폭발
+    public GameObject explosionEffectPrefab; // 이펙트
+
+    private bool hasExploded = false; // 두 번 방지
+    
+    // 충돌시 폭발
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 닿았을 때 터지기
+        if (hasExploded) return; 
+        if (collision.gameObject.CompareTag("Player")) return; 
+
+        Explode();
+    }
+
+    public virtual void Explode()
+    {
+        hasExploded = true; // 터지는 거 체크
+
+        // 이펙트 미리 준비
+        if (explosionEffectPrefab != null)
+        {
+            Instantiate(explosionEffectPrefab, transform.position, Quaternion.identity);
+        }
+
+        // 주변 물체 밀기
+        Collider[] colliders = Physics.OverlapSphere(transform.position, EffectRange); // 범위 내 물체 찾기
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                // 터질 때 밀어내기
+                rb.AddExplosionForce(ExplosionForce, transform.position, EffectRange, 2.0f);
+            }
+            
+
+        }
+
+        Debug.Log("수류탄 폭발");
+
+        // 수류탄 삭제
+        Destroy(gameObject);
+    }
+}
---
+++ ProjectAIF/Assets/Resources/WeaponTest/Prefab/WeaponID.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponID : MonoBehaviour
+{
+    public WeaponType ID;
+}
+
+public enum WeaponType
+{
+    BasePistol,    // 권총
+    BaseRifle,     // 소총
+    BaseGrenade    // 수류탄
+}
