d07ec3d0ffe7f7111acca005731f702797f7be01
vvCapella <chlrudtjs368@gmail.com>
2026-01-29 13:58:24 +0900

    크리스탈에 마우스 접촉시 윤곽선 활성화
    너무 멀리서는 작동되지않게 레이캐스트에 maxDistanse 를 사용했습니다. 윤곽선 색은 흰색인데 무난하니 괜찮네요!

--- ProjectAIF/Assets/Scenes/ChoigsWorkSpace/CrystalSkill.cs
+++ ProjectAIF/Assets/Scenes/ChoigsWorkSpace/CrystalSkill.cs
@@ -4,36 +4,19 @@ using UnityEngine;
 
 public class CrystalSkill : MonoBehaviour
 {
-    // 전멸시킬 적 태그 이름
     public string enemyTag = "Enemy";
 
-    // 웨이브당 딱 한번 제한
     private bool used = false;
 
-    // 마우스 클릭시 실행
-    private void OnMouseDown()
+    public void Activate()
     {
-        // 5초 후 스킬 발동, 전멸 코루틴 시작 
-        StartCoroutine(KillAllAfterDelay());
+        if (used) return;
+        used = true;
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        foreach (var e in enemies)
+            Destroy(e);
     }
-    // 시간지연 가능한 함수 
-    IEnumerator KillAllAfterDelay()
-    {
-        Debug.Log("전방위 스킬 발동");
 
 
-        // 발동 5초 딜레이
-        yield return new WaitForSeconds(5f);
-
-        // Enemy 태그를 가진 몬스터 전부 찾기
-        GameObject[] Monsters = GameObject.FindGameObjectsWithTag("Enemy");
-        
-        // 전부 제거하기
-        foreach (GameObject monster in Monsters)
-        {
-            Destroy(monster);
-        }
-
-        Debug.Log("강대한 힘으로 몬스터 전멸");
-    }
 }
