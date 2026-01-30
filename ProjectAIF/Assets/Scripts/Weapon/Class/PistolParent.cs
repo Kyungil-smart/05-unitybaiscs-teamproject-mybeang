3a22601fa0265efad6d82bd9326e32ffce3360d9
hjchun78-cmd <hjchun78@gmail.com>
2026-01-30 11:04:31 +0900

    [edit] 경로변경 및 정리
    Resources에 있던 제작본들을 옮겼습니다.

---
+++ ProjectAIF/Assets/Scripts/Weapon/Class/PistolParent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    [Header("권총")]
+    public int Damage; //데미지
+    [Range(0f, 1f)]
+    public float CriticalChance; // 치명타 확률
+    public int CriticalDamage; //치명타 데미지
+    public int Magazine; //장탄수
+    public float AttackRate; //공격 속도
+
+    public virtual void Fire()
+    {
+        Debug.Log("권총 사격");
+    }
+}
---
+++ ProjectAIF/Assets/Scripts/Weapon/Class/RifleParent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rifle : MonoBehaviour
+{
+    [Header("소총")]
+    public int Damage; // 기본 데미지
+    [Range(0f, 1f)]
+    public float CriticalChance; //치명타 확률
+    public int CriticalDamage; //치명타 데미지
+    public int Magazine; //장탄수
+    public float AttackRate; // 사격 속도
+
+    public virtual void Fire()
+    {
+        Debug.Log("소총 발사");
+    }
+}
