93528a7ec4f19e5a468341939206b3fbe6606d1d
vvCapella <chlrudtjs368@gmail.com>
2026-02-02 17:55:34 +0900

    리스폰시 사운드 추가
    리스폰시 크리스탈 목소리가 재생됩니다.

--- ProjectAIF/Assets/Scripts/Player/PlayerRespawn.cs
+++ ProjectAIF/Assets/Scripts/Player/PlayerRespawn.cs
@@ -6,9 +6,16 @@ public class PlayerRespawn : MonoBehaviour
 {
     [SerializeField] private Transform _playerTf;
     [SerializeField] private Transform _respawnPoint;
+    [SerializeField] private AudioClip _playerRespawnSound;
+
     public void PlayerToRespawnPoint() 
     {
         _playerTf.position = _respawnPoint.position;
+        AudioManager.Instance.PlaySound(_playerRespawnSound);
     }
-        
+
+
+    
+
+
 }
